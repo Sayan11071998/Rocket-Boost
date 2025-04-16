2c00d6584b4fa2ccb9b8cdefa14c480da13f8cc6
Sayan Nandi <sayannandi11071998@gmail.com>
2025-04-17 01:03:04 +0530

    Rotation Added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private InputAction thrust;
+    [SerializeField] private InputAction rotation;
+
     [SerializeField] private float thrustStrength = 100f;
+    [SerializeField] private float rotationStrength = 100f;
 
     private Rigidbody rb;
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
     }
 
     private void Start()
@@ -20,9 +25,34 @@ public class Movement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
+    {
         if (thrust.IsPressed())
         {
             rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
     }
 }
